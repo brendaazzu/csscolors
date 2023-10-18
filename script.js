1293e1c42c89c78c14e89f97c028ebe0abfd75ca
Brenda Azzu <100886386+brendaazzu@users.noreply.github.com>
2023-10-17 22:45:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*    var texto = 'Hello World (≧∇≦)';
+var div = document.getElementById('texto');
+var i = 0;
+
+function typeWriter() {
+    if (i < texto.length) {
+        div.innerHTML += texto.charAt(i);
+        i++;
+        setTimeout(typeWriter, 110); 
+    }
+}
+
+typeWriter(); */
